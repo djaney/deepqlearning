306e3ae0cfd4e8a8de568a0e89fa84aa07c71abb
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-08-03 21:29:36 +0800

    stuff

---
+++ atari_ram.py
@@ -0,0 +1,60 @@
+import numpy as np
+import gym
+import sys
+import os
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+
+ENV_NAME = 'Breakout-ram-v0'
+WEIGHTS_PATH = '.models/dqn_{}_weights.h5f'.format(ENV_NAME)
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
+
+# Next, we build a very simple model.
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+model.summary()
+
+
+train_mode = len(sys.argv) > 1 and sys.argv[1] == 'train'
+
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=20000, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+               target_model_update=1e-2, policy=policy, enable_dueling_network=True, dueling_type='avg',)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+if os.path.isfile(WEIGHTS_PATH) and os.access(WEIGHTS_PATH, os.R_OK):
+    dqn.load_weights(WEIGHTS_PATH)
+
+if train_mode:
+
+    dqn.fit(env, nb_steps=999999999, visualize=False, verbose=2)
+    dqn.save_weights(WEIGHTS_PATH, overwrite=True)
+    print('save')
+
+
+else:
+    # Finally, evaluate our algorithm for 5 episodes.
+    dqn.test(env, nb_episodes=5, visualize=True)
