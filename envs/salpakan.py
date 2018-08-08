955787e55f4005e8cf83f61a6e20fe150ad34402
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-08-08 22:29:59 +0800

    stuff

--- envs/salpakan.py
+++ envs/salpakan.py
@@ -2,18 +2,25 @@ from gym import Env, spaces
 import numpy as np
 from .salpakan_game import SalpakanGame
 
+OBSERVATION_SHAPE = (9, 8, 3)
 
 class SalpakanEnv(Env):
 
     def __init__(self):
-        self.observation_space = spaces.Box(low=0, high=1, shape=(9, 8, 5), dtype=np.float16)
-        self.action_space = spaces.Discrete(85)
+
+        self.observation_space = spaces.Box(low=0, high=1, shape=OBSERVATION_SHAPE, dtype=np.float16)
+        self.action_space = spaces.Discrete(8*9*4+1)
+        self.game = None
 
     def step(self, action):
-        pass
+        reward = self.game.move(action)
+        ob = self._get_state()
+        done = self.game.winner is not None
+        return ob, reward, done, {}
 
     def reset(self):
-        pass
+        self.game = SalpakanGame()
+        return self._get_state()
 
     def render(self, mode='human'):
         pass
@@ -23,3 +30,21 @@ class SalpakanEnv(Env):
 
     def seed(self, seed=None):
         return super().seed(seed)
+
+    def _get_state(self):
+
+        observation = np.zeros(shape=OBSERVATION_SHAPE)
+
+        board = self.game.get_board()
+
+        my_troops = np.clip(board[:, :, 0], 0, None)
+        enemy_troops = np.clip(board[:, :, 0] * -1, 0, None)
+
+        # enemy perception, flip and clip, troops channel
+        observation[:, :, 0] = np.clip(enemy_troops, 0, 1) * board[:, :, 1]
+        # my units, clip troops channel
+        observation[:, :, 1] = my_troops
+        # my spies
+        observation[:, :, 2] = np.clip(my_troops, 0, 1) * board[:, :, 2]
+
+        return observation
--- envs/salpakan_game.py
+++ envs/salpakan_game.py
@@ -9,13 +9,20 @@ SPECIAL_MOVES_N = 1
 
 MOVE_NORMAL = 0
 MOVE_CAPTURE = 1
-MOVE_WIN = 2
+MOVE_WIN = 10
+MOVE_PASS = 0
 
 CHANNEL_TROOPS = 0
 CHANNEL_PERCEPTION = 1
+CHANNEL_SPY_PERCEPTION = 2
 
 PLAYER_1 = 0
 PLAYER_2 = 1
+
+TROOP_FLAG = 1
+TROOP_SPY = 2
+TROOP_PRIVATE = 3
+TROOP_FIVE_STAR = 15
 
 
 def _parse_move(move):
@@ -42,10 +49,11 @@ def _parse_move(move):
 
 
 def _normalize_board(player, board):
+    # TODO bug onlt returning 1 channel
     if player < 0:
-        return board * -1
+        return board[:, :, CHANNEL_TROOPS] * -1
     else:
-        return board * 1
+        return board[:, :, CHANNEL_TROOPS] * 1
 
 
 class SalpakanGame:
@@ -53,13 +61,15 @@ class SalpakanGame:
         # Create board
         # width x height x channels
         # channels = troops, perception
-        self.board = np.zeros((9, 8, 2))
+        self.board = np.zeros((9, 8, 3))
 
         # generate troops
         self._generate_troops(0)
         self._generate_troops(1)
 
         self.turn = random.randint(0, 1)
+
+        self.winner = None
 
     def _generate_troops(self, player):
         assert player == PLAYER_1 or player == PLAYER_2
@@ -75,34 +85,76 @@ class SalpakanGame:
             for r in range(p):
                 for _ in range(36):
                     y = random.randint(y_min, y_max)
-                    x = random.randint(0, WIDTH-1)
+                    x = random.randint(0, WIDTH - 1)
                     # if no troop
                     if self.board[x][y][CHANNEL_TROOPS] == 0:
                         # set troops, negative if player 2
-                        self.board[x][y][CHANNEL_TROOPS] = i+1 if player == PLAYER_1 else (i+1)*-1
+                        self.board[x][y][CHANNEL_TROOPS] = i + 1 if player == PLAYER_1 else (i + 1) * -1
+                        # set spy flag
+                        if self.board[x][y][CHANNEL_TROOPS] == TROOP_SPY:
+                            self.board[x][y][CHANNEL_SPY_PERCEPTION] = 1
                         break
 
-    def move(self, player, move):
+    def move(self, move):
         """
         direction: 0=left, 1=right, 2=top, 3=bottom
         :param player:
         :param move:
         :return:
         """
-        assert self.turn == player
+        player = self.turn
 
         if move != 0:
             square_id, x, y, _x, _y, direction = _parse_move(move)
 
             assert self._is_valid_move(player, (x, y), (_x, _y))
 
-            # TODO
-            # do move
+            move_type = self._get_move_type(player, (x, y), (_x, _y))
+
+            if move_type == MOVE_NORMAL:
+                tmp = self.board[x][y]
+                self.board[x][y] = self.board[_x][_y]
+                self.board[_x][_y] = tmp
+            elif move_type == MOVE_CAPTURE:
+                normalized_board = _normalize_board(player, self.board)
+                me = normalized_board[x][y][CHANNEL_TROOPS]
+                him = normalized_board[_x][_y][CHANNEL_TROOPS]
+
+                if me == him:  # cancel out
+                    win = 0
+                elif me == TROOP_SPY and him != -TROOP_PRIVATE:  # spy captures
+                    win = 1
+                elif me == TROOP_PRIVATE and him == -TROOP_SPY:  # private captures spy
+                    win = 1
+                else:  # normal rank based clash
+                    win = 1 if me > him else -1
+
+                if win > 0:  # win
+                    self.board[_x][_y] = self.board[x][y]
+                    self.board[x][y] = self.board[x][y] * 0
+                elif win < 0:  # lose
+                    self.board[_x][_y][CHANNEL_PERCEPTION] = max(self.board[x][y][CHANNEL_TROOPS]+1,
+                                                                 self.board[_x][_y][CHANNEL_PERCEPTION])
+                    self.board[x][y] = self.board[x][y] * 0
+                else:
+                    self.board[_x][_y][CHANNEL_PERCEPTION] = self.board[x][y][CHANNEL_TROOPS]
+                    self.board[x][y] = self.board[x][y] * 0
+                    self.board[_x][_y] = self.board[_x][_y] * 0
+            elif move_type == MOVE_WIN:
+                self.board[_x][_y] = self.board[x][y]
+                self.board[x][y] = self.board[x][y] * 0
+                self.winner = player
+        else:
+            move_type = MOVE_PASS
 
         # change turn
         self.turn = PLAYER_2 if self.turn == PLAYER_1 else PLAYER_1
 
         # return move type
+        return move_type
+
+    def get_board(self):
+        return _normalize_board(self.turn, self.board)
 
     def _is_valid_move(self, player, src, destination):
         board = _normalize_board(player, self.board)
@@ -121,3 +173,10 @@ class SalpakanGame:
         board = _normalize_board(player, self.board)
         me = board[src[0]][src[1]][CHANNEL_TROOPS]
         him = board[destination[0]][destination[1]][CHANNEL_TROOPS]
+
+        if me > 0 and him == -TROOP_FLAG:
+            return MOVE_WIN
+        elif me > 0 and 0 > him:
+            return MOVE_CAPTURE
+        else:
+            return MOVE_NORMAL
